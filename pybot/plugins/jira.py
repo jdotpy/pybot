e07e8bd9188c2595b9b7a21af32c7f832a111546
kj <redacted>
2016-04-15 09:35:39 -0400

    Jira issue mention verbose mode

--- pybot/plugins/jira.py
+++ pybot/plugins/jira.py
@@ -1,14 +1,14 @@
 from .core import HearMessagePlugin
 import requests
 
-class JiraHelpers(HearMessagePlugin):
+class IssueMention(HearMessagePlugin):
     hear_regexes = [
-        r'#(\d{1,6})\b',
-        r'\b(\w{2,50}\-\d{1,6})\b'
+        r'#(\d{1,6})( -v)?\b',
+        r'\b(\w{2,50}\-\d{1,6})( -v)?\b'
     ]
 
     def __init__(self, *args, **kwargs):
-        super(JiraHelpers, self).__init__(*args, **kwargs)
+        super(IssueMention, self).__init__(*args, **kwargs)
         self.base_url = self.options.get('url', None)
         self.default_project = self.options.get('default_project', None)
 
@@ -27,6 +27,7 @@ class JiraHelpers(HearMessagePlugin):
 
     def hear(self, message, match=None):
         issue_id = match.group(1)
+        verbose = ' -v' in match.groups()
         if '-' not in issue_id:
             if self.default_project:
                 issue_id = self.default_project + '-' + issue_id
@@ -36,11 +37,30 @@ class JiraHelpers(HearMessagePlugin):
             'expand': 'summary'
         })
         if results is None:
-            print('didnt find a ticket')
             return None
         ticket_url = self.base_url + '/browse/' + issue_id
         summary = results['fields']['summary']
+        if results['fields']['description']:
+            details = '\n' + results['fields']['description']
+        else:
+            details = ''
         status = results['fields']['status']['name']
         issue_type = results['fields']['issuetype']['name']
         assigned_to = results['fields']['assignee']['displayName']
-        return '[{} {}] {} @{}\n{}'.format(status, issue_type, summary, assigned_to, ticket_url)
+        creator = results['fields']['creator']['displayName']
+        created = results['fields']['created'].replace('T', ' ')
+        comments = results['fields']['comment']['comments']
+        if comments:
+            last_comment_date = comments[-1]['created'].replace('T', ' ')
+        else:
+            last_comment_date = 'No comments'
+        if verbose:
+            return '[{} {}] {}\nCreated by: {}\nAssigned to: {}\nLast Comment: {}\n{}{}'.format(
+                status, issue_type, summary, 
+                creator, assigned_to, 
+                last_comment_date,
+                ticket_url,
+                details
+            )
+        else:
+            return '[{} {}] {} @{}\n{}'.format(status, issue_type, summary, assigned_to, ticket_url)
