caf98ecbe4a2bde68578411520577a17b415f174
kj <redacted>
2016-04-18 09:17:09 -0400

    Added chuck norris jokes

---
+++ pybot/plugins/funny.py
@@ -0,0 +1,17 @@
+from .core import SimpleResponder
+
+class ChuckNorris(SimpleResponder):
+    hear_regex = r'tell me about (\w+) (\w+)'
+    base_url = 'http://api.icndb.com/jokes/random'
+
+    def hear(self, message, match=None):
+        first_name, last_name = match.groups()
+        params = {
+            'firstName': first_name,
+            'lastName': last_name
+        }
+        result_code, response = self.bot.web('GET', self.base_url, params=params)
+        if result_code:
+            return response.json()['value']['joke']
+        else:
+            return 'Chuck norris error: ' + str(response)
